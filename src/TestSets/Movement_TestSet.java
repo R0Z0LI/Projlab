dc252b13353f1c3902eff06af21551e6dba31b53
Levente Budavári <budavarilevente@edu.bme.hu>
2022-03-26 18:46:32 +0100

    Movement_TestSet

---
+++ src/TestSets/Movement_TestSet.java
@@ -0,0 +1,78 @@
+package TestSets;
+
+import agent.*;
+import behaviors.*;
+import virologist.Virologist;
+import Field.Field;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Movement_TestSet {
+
+    private Agent usedAgent;
+    private Virologist applyingViro;
+    private Virologist affectedViro;
+
+    // constructor
+    public Movement_TestSet() {
+        init_test();
+    }
+
+    // testing use case
+    public void test() {
+
+    }
+
+    // initializing everything we need for this test
+    private void init_test() {
+
+        Virologist virologist = new Virologist();
+        Field jelenlegi = new Field();
+        MovementBehavior movementBehavior = new MovementBehavior(virologist);
+
+        ArrayList neighbours = jelenlegi.getNeighbours();
+        Field cél = (Field)neighbours.get(0);
+
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Melyik mozgást teszteljük? Sima / Crazy / Stunned");
+            String MovementType = sc.next();
+            switch (MovementType) {
+                case "Sima":
+                    movementBehavior.Move(jelenlegi, cél);
+
+                    virologist.setCurrField(cél);
+                    jelenlegi.RemoveVirologist(virologist);
+                    cél.AddVirologist(virologist);
+                    break;
+
+                case "Crazy":
+                    CrazyMoveBehavior.Move(jelenlegi, cél);
+
+                    virologist.setCurrField(cél);
+                    jelenlegi.RemoveVirologist(virologist);
+                    cél.AddVirologist(virologist);
+                    break;
+
+                case "Stunned":
+                    StunnedMoveBehavior.Move(jelenlegi, cél);
+                    break;
+
+                default:
+                    System.err.println("Wrong input! We choose Sima 4 u");
+                    MovementType = "Sima";
+                    break;
+
+        }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
