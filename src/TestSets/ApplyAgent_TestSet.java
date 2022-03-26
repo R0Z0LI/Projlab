c764a588da8400c501d1df1e7c35630a5939b478
Dániel Veress <veressdaniel@edu.bme.hu>
2022-03-26 20:27:44 +0100

    Zoli kívánsága.

--- src/TestSets/ApplyAgent_TestSet.java
+++ src/TestSets/ApplyAgent_TestSet.java
@@ -1,5 +1,6 @@
 package TestSets;
 
+import PropertyHandler.PropertyHandler;
 import agent.*;
 import behaviors.*;
 import equipments.Cape;
@@ -22,9 +23,7 @@ public class ApplyAgent_TestSet {
 
     // testing use case
     public void test() {
-
         applyingViro.applyAgent(usedAgent, affectedViro);
-
     }
 
     // initializing everything we need for this test
@@ -33,7 +32,8 @@ public class ApplyAgent_TestSet {
         Field field = new Field();
 
         // setting up applying virologist
-        applyingViro = new Virologist();
+        PropertyHandler ph1 = new PropertyHandler(3, 10, 10, applyingViro);
+        applyingViro = new Virologist(2, ph1, null);
         applyingViro.setApplyBehaviors(new ApplyBehavior(applyingViro));
         applyingViro.setCurrField(field);
 
@@ -62,34 +62,35 @@ public class ApplyAgent_TestSet {
 
 
         System.out.print("Egy másik virológusra kenje? [i/n] ");
-        if (sc.next() == "i") {
+        if (sc.next().equals("i")) {
             // setting up affected virologist
-            affectedViro = new Virologist();
+            PropertyHandler ph2 = new PropertyHandler(3, 10, 10, affectedViro);
+            affectedViro = new Virologist(2, ph2, null);
             affectedViro.setDefenseBehaviors(new DefenseBehavior(affectedViro));
             affectedViro.setCurrField(field);
 
             // setting up defenses for both virologists
 
             System.out.print("Van kesztyűje a másik virológusnak? [i/n] ");
-            if (sc.next() == "i") {
+            if (sc.next().equals("i")) {
                 affectedViro.getPropertyHandler().AddEquipment(new Gloves());
                 affectedViro.setDefenseBehaviors(new GloveDefBehavior(affectedViro));
 
                 System.out.print("Van kesztyűje a támadó virológusnak? [i/n] ");
-                if (sc.next() == "i") {
+                if (sc.next().equals("i")) {
                     applyingViro.getPropertyHandler().AddEquipment(new Gloves());
                     applyingViro.setDefenseBehaviors(new GloveDefBehavior(applyingViro));
                 }
             }
 
             System.out.print("Hat védő ágens  a másik virológusra? [i/n] ");
-            if (sc.next() == "i") {
+            if (sc.next().equals("i")) {
                 affectedViro.getPropertyHandler().AddAgent(new ProtectionAgent(30));
                 affectedViro.setDefenseBehaviors(new DefAgentDefBehavior(affectedViro));
             }
 
             System.out.print("Van védőköpenye  a másik virológusnak? [i/n] ");
-            if (sc.next() == "i") {
+            if (sc.next().equals("i")) {
                 affectedViro.getPropertyHandler().AddEquipment(new Cape());
                 affectedViro.setDefenseBehaviors(new CapeDefBehavior(affectedViro));
             }
