71ce0f0fd1d05167bcb9bd4dc5e9520a9109974f
Dániel Veress <veressdaniel@edu.bme.hu>
2022-04-15 16:25:29 +0200

    Field osztály megcsinálva.

--- src/Field/Field.java
+++ src/Field/Field.java
@@ -3,50 +3,69 @@ package Field;
 import Virologist.Virologist;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A játék egy mezőjét reprezentáló osztály
  */
 public class Field {
-    private List<Virologist> virologists = new ArrayList<>();
+    private ArrayList<Virologist> virologists = new ArrayList<>();
     private ArrayList<Field> neighbours = new ArrayList<>();
 
-    //TODO kell-e konstruktor szomszédokkal
-
+    // empty constructor
     public Field() {
-
+        //
     }
+
+    // TODO more complex constructor? (maybe)
 
     /**
      * virologists getterje
      *
-     * @return visszaadja a virológusok listáját
+     * @return visszaadja a mezőről elérhető virológusok listáját
      */
-    public List<Virologist> GetTouchableVirologists() {
-        System.out.println("-> GetTouchableVirologists()\n! virologists getterje\n <- return virologists\n\n");
+    public ArrayList<Virologist> GetTouchableVirologists() {
+        //System.out.println("-> GetTouchableVirologists()\n! virologists getterje\n <- return virologists\n\n");
         return virologists;
     }
 
-    //TODO remove és addVirologist metódusokat egy metódusba tenni
+    /**
+     * Hozzáadja a paraméterként megadott virológust a mezőn lévő virológusokhoz
+     *
+     * @param virologist Ezt adja hozzá
+     */
+    public void addVirologist(Virologist virologist) {
+        //System.out.println("-> AddVirologist(Virologist virologsit)\n! Hozzaadja a parameterkent megadott virologust a mezon levo virologusokhoz\n\n");
+        virologists.add(virologist);
+        System.out.println("\tA new virologist has been given.");
+    }
+
     /**
      * Kitörli a paraméterként megadott virológust a mezőn lévő virológusok közül
      *
      * @param virologist Ezt a virológust törli ki
      */
     public void removeVirologist(Virologist virologist) {
-        System.out.println("-> RemoveVirologist(Virologist Virologist)\n! Kitorli a parameterkent megadott virologust a mezon levo virologusok kozul\n\n");
+        //System.out.println("-> RemoveVirologist(Virologist Virologist)\n! Kitorli a parameterkent megadott virologust a mezon levo virologusok kozul\n\n");
         virologists.remove(virologist);
+        System.out.println("\tThe virologist stepped off from this field.");
     }
 
     /**
-     * Hozzáadja a paraméterként megadott virológust a mezőn lévő virológusokhoz
+     * Hozzáad egy szomszédos mezőt
      *
-     * @param virologsit Ezt adja hozzá
+     * @param field Ezt a mezőt adja hozzá szomszédként
      */
-    public void addVirologist(Virologist virologsit) {
-        System.out.println("-> AddVirologist(Virologist virologsit)\n! Hozzaadja a parameterkent megadott virologust a mezon levo virologusokhoz\n\n");
-        virologists.add(virologsit);
+    public void addNeighbour(Field field) {
+        neighbours.add(field);
+        System.out.println("\tA new neighbour has been given.");
+    }
+
+    /**
+     * Anyagot pusztít a mezőn, mivel ez egy sima mező, ezért itt nem történik semmi
+     */
+    public void rampage() {
+        //
+        System.out.println("\tThe virologist tried to rampage on a normal field.");
     }
 
     /**
@@ -55,11 +74,8 @@ public class Field {
      * @return A szomszédos mezők listáját adja vissza
      */
     public ArrayList<Field> getNeighbours() {
-        System.out.println("-> getNeighbours()\n! Visszaadja a szomszedos mezoket\n <- neighbours\n\n");
+        //System.out.println("-> getNeighbours()\n! Visszaadja a szomszedos mezoket\n <- neighbours\n\n");
         return neighbours;
     }
 
-    public void addNeighbour(Field field) {
-        neighbours.add(field);
-    }
 }
