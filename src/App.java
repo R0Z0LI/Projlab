ab0fcbdd82be67f820c6b950d61431683d307edd
Anna <strbikan@gmail.com>
2022-04-22 22:15:24 +0200

    TODO a tesztekhez

--- src/App.java
+++ src/App.java
@@ -3,9 +3,11 @@ import TestSets.TestInOutHandler;
 
 public class App {
     public static void main(String[] args){
+        //TODO megadni a bemeneti fájlt inicializáláshoz és parancsokhoz pl konstruktorban
         Game game = new Game();
         game.start();
         TestInOutHandler testHandler = new TestInOutHandler();
+        //TODO a többi tesztnél még testHandler.setOutputFile(fájlnév);
         testHandler.writeInFile();
     }
 }
